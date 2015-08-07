297693d3f07adefac1830dfc4270e10a4d0f6780
jeffersonomonteiro <jeffersonmonteiro485@gmail.com>
2015-08-07 19:19:59 -0300

    adicionando nova condição ao scrip telefonia

--- telefonia.py
+++ telefonia.py
@@ -1,14 +1,18 @@
 minutos = int(input("minutos : "))
 if minutos < 200:
     preco = minutos * 0.20
-    print ( "valor da conta : %.2f" %preco)
+    
 else:
     if minutos >= 200 and minutos <=400:
          preco = minutos * 0.18    
-         print( "valor da conta : %.2f" %preco)
-        
+          
     else:
-         preco = minutos * 0.15
-         print("valor da conta : %.2f" %preco)
-        
-    
+        if minutos >= 800:
+            preco = minutos * 0.08
+        else:
+            preco = minutos * 0.15
+
+
+
+print("valor da conta : %.2f" %preco)
+
